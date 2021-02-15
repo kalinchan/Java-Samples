c4d9270fe0c3952e49ec5e90b228c3ca86c3db96
Kalin Chan <kalin.chan@payara.fish>
2021-02-15 12:10:08 +0000

    Added some comments

--- concurrency/threads/src/main/java/io/github/kalinchan/threads/DeadlockExample.java
+++ concurrency/threads/src/main/java/io/github/kalinchan/threads/DeadlockExample.java
@@ -43,3 +43,15 @@ public class DeadlockExample {
 }
 //When Deadlock runs, it's extremely likely that both threads will block when they attempt to invoke bowBack. 
 //Neither block will ever end, because each thread is waiting for the other to exit bow.
+
+//Starvation describes a situation where a thread is unable to gain regular access to shared resources and is 
+//unable to make progress. This happens when shared resources are made unavailable for long periods by "greedy" 
+//threads. For example, suppose an object provides a synchronized method that often takes a long time to return. 
+//If one thread invokes this method frequently, other threads that also need frequent synchronized access to the 
+//same object will often be blocked.
+
+//A thread often acts in response to the action of another thread. 
+//If the other thread's action is also a response to the action of another thread, then livelock may result. 
+//As with deadlock, livelocked threads are unable to make further progress.
+//However, the threads are not blocked — they are simply too busy responding to each other to resume work.
+
